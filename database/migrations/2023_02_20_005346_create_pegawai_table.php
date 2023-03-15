c98b20cd622fe15de8a733380913c91c2d8cfe3d
ricotaffarel <ricotaffarel1111@gmail.com>
2023-03-15 08:13:07 +0700

    baru

---
+++ database/migrations/2023_02_20_005346_create_pegawai_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePegawaiTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pegawai', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->char('gender');
+            $table->string('photo')->nullable();
+            $table->char('status_user');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('address');
+    }
+}
